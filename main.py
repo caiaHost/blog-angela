189d10dea0e87a8d7036ac823c51ef57b886f6f5
repl.it user <replituser@example.com>
2022-07-25 16:10:16 +0200

    Upgrade v.8 added stuff

--- main.py
+++ main.py
@@ -226,6 +226,10 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
+@app.route("/stuff")
+def stuff():
+    return render_template("pandas-introduction-youtube-tutorial.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
